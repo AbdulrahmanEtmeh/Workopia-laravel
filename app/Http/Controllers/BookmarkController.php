83c45183ff3b92f51895b541d6da9832216eab93
Abdulrahman <etwy.2000@gmail.com>
2026-04-10 17:58:23 +0300

    Remove Bookmark

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -21,7 +21,7 @@ class BookmarkController extends Controller
     }
 
     // @desc    Create new bookmarked job
-    // @route   POST /bookmarks
+    // @route   POST /bookmarks/{job}
     public function store(Job $job): RedirectResponse
     {
         $user = Auth::user();
@@ -35,5 +35,22 @@ class BookmarkController extends Controller
         $user->bookmarkedJobs()->attach($job->id);
 
         return back()->with('success', 'Job bookmarked successfully!');
+    }
+
+    // @desc    Remove bookmarked job
+    // @route   DELETE /bookmarks/{job}
+    public function destroy(Job $job): RedirectResponse
+    {
+        $user = Auth::user();
+
+        // Check if the job is not bookmarked
+        if (!$user->bookmarkedJobs()->where('job_id', $job->id)->exists()) {
+            return back()->with('error', 'Job is not bookmarked');
+        }
+
+        // Remove bookmark
+        $user->bookmarkedJobs()->detach($job->id);
+
+        return back()->with('success', 'Bookmark removed successfully!');
     }
 }
